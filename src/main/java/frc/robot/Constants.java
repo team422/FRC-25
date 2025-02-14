2cc924293c62a77aff3de02036f316a7dd3c91e2
keckothedragon <flappystar05@gmail.com>
2025-02-14 15:10:24 -0500

    Update to 2025.3.1 and update vendordeps

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -229,7 +229,7 @@ public final class Constants {
 
   public static final class AprilTagVisionConstants {
     public static final AprilTagFieldLayout kAprilTagLayout =
-        AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+        AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark);
     public static final double kAprilTagWidth = Units.inchesToMeters(6.5);
 
     public static final double kAmbiguityThreshold = 0.4;
