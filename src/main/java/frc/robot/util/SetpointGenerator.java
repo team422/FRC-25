fee16ca8841bdbf4d3958449a3944ec5b99dce9b
Toomy422 <26mwood@gsgis.k12.va.us>
2025-11-23 12:57:53 -0500

    Autoscore meets Transform2d

--- src/main/java/frc/robot/util/SetpointGenerator.java
+++ src/main/java/frc/robot/util/SetpointGenerator.java
@@ -2,7 +2,7 @@ package frc.robot.util;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import frc.lib.littletonUtils.AllianceRotateUtil;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.FieldConstants.Reef;
@@ -14,44 +14,39 @@ import org.littletonrobotics.junction.Logger;
 public class SetpointGenerator {
   private SetpointGenerator() {}
 
-  private static ArrayList<Pose2d> m_leftPoles = new ArrayList<>();
-  private static ArrayList<Pose2d> m_rightPoles = new ArrayList<>();
   private static ArrayList<Pose2d> m_leftPoses = new ArrayList<>();
   private static ArrayList<Pose2d> m_rightPoses = new ArrayList<>();
 
   static {
+    ArrayList<Pose2d> leftPoles = new ArrayList<>();
+    ArrayList<Pose2d> rightPoles = new ArrayList<>();
+
     int i = 0;
     for (Map<ReefHeight, Pose2d> poses : Reef.branchPositions2d) {
       if (i % 2 == 0) {
-        m_rightPoles.add(poses.get(ReefHeight.L1));
+        rightPoles.add(poses.get(ReefHeight.L1));
       } else {
-        m_leftPoles.add(poses.get(ReefHeight.L1));
+        leftPoles.add(poses.get(ReefHeight.L1));
       }
       i++;
     }
 
-    System.out.println(m_leftPoles.size());
+    System.out.println(leftPoles.size());
 
-    for (Pose2d pose : m_leftPoles) {
-      m_leftPoses.add(AllianceRotateUtil.apply(
-          new Pose2d(
-              pose.getTranslation()
-                  .plus(
-                      new Translation2d(
-                          DriveConstants.kTrackWidthX * Math.cos(pose.getRotation().getRadians()),
-                          DriveConstants.kTrackWidthY * Math.sin(pose.getRotation().getRadians()))),
-              pose.getRotation().rotateBy(Rotation2d.k180deg))));
+    for (Pose2d pole : leftPoles) {
+      var pose =
+          pole.transformBy(
+              new Transform2d(
+                  DriveConstants.kTrackWidthX, 0, new Rotation2d().rotateBy(Rotation2d.k180deg)));
+      m_leftPoses.add(AllianceRotateUtil.apply(pose));
     }
 
-    for (Pose2d pose : m_rightPoles) {
-      m_rightPoses.add(AllianceRotateUtil.apply(
-          new Pose2d(
-              pose.getTranslation()
-                  .plus(
-                      new Translation2d(
-                          DriveConstants.kTrackWidthX * Math.cos(pose.getRotation().getRadians()),
-                          DriveConstants.kTrackWidthY * Math.sin(pose.getRotation().getRadians()))),
-              pose.getRotation().rotateBy(Rotation2d.k180deg))));
+    for (Pose2d pole : rightPoles) {
+      var pose =
+          pole.transformBy(
+              new Transform2d(
+                  DriveConstants.kTrackWidthX, 0, new Rotation2d().rotateBy(Rotation2d.k180deg)));
+      m_rightPoses.add(AllianceRotateUtil.apply(pose));
     }
   }
 
@@ -63,7 +58,7 @@ public class SetpointGenerator {
     } else {
       newest = pose.nearest(m_rightPoses);
     }
-    Logger.recordOutput("nearest", newest);
+    Logger.recordOutput("Autoscore/nearestPose", newest);
     return newest;
   }
 }
